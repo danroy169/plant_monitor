e51e82b88de3c99b02606dd275192a7d9c798438
Daniel Roy <daniel.roy@us.bosch.com>
2021-04-06 15:29:52 -0400

    refactor

--- emailer.py
+++ emailer.py
@@ -1,32 +1,21 @@
 import smtplib
 
-port = 465  # For SSL
-smtp_server_name = "smtp.gmail.com"
-password = "thisisapassword"
-this_email = "royplantmonitor@gmail.com"
-email_to = "danielwroy90@gmail.com"
+PORT = 465  # For SSL
+SMTP_SERVER_NAME = "smtp.gmail.com"
+PASSWORD = "thisisapassword"
+SEND_EMAIL = "royplantmonitor@gmail.com"
+RECIEVE_EMAIL = "danielwroy90@gmail.com"
 
 
 def send_email(moisture):
-    
+
     message = """\
         Subject: Plant Alert
 
         This message is sent from the Raspberry Pi Plant Monitor.
-        \nSoil moisture latest reading is """ + str(moisture)
-    
-    if(moisture < 400):
-        message += ". That is too low. Please water"
+        \nSoil moisture latest reading is """ + str(moisture) + " please water ASAP"
 
-
-    connection = smtplib.SMTP_SSL(smtp_server_name, port)
-    connection.login(this_email, password)
-    connection.sendmail(this_email, email_to, message)
+    connection = smtplib.SMTP_SSL(SMTP_SERVER_NAME, PORT)
+    connection.login(SEND_EMAIL, PASSWORD)
+    connection.sendmail(SEND_EMAIL, RECIEVE_EMAIL, message)
     connection.close()
-
-    
-
-
-
-
-
--- test1.py
+++ test1.py
@@ -1,44 +1,44 @@
 import time
 import sqlite3
-from sqlite3 import Error 
+from sqlite3 import Error
 from board import SCL, SDA
 import busio
 from db_connect import create_connection, write_to_db
 from emailer import send_email
- 
 from adafruit_seesaw.seesaw import Seesaw
- 
-i2c_bus = busio.I2C(SCL, SDA)
- 
-ss = Seesaw(i2c_bus, addr=0x36)
+
+I2C_BUS = busio.I2C(SCL, SDA)
+
+SS = Seesaw(I2C_BUS, addr=0x36)
 
 DB = "/home/pi/Projects/Plant Monitor/db/plants.db"
 DATE_FORMAT = '%m/%d/%Y'
 TIME_FORMAT = '%H:%M:%S'
 
+
 def cel_to_far(cel):
-     return (cel * 1.8) + 32
+    return (cel * 1.8) + 32
+
 
 while True:
     conn = create_connection(DB)
-    # read moisture level through capacitive touch pad
-    moisture = ss.moisture_read()
- 
-    # read temperature from the temperature sensor
-    temp = int(cel_to_far(ss.get_temp()))
-    
+
+    moisture = SS.moisture_read()
+
+    temp = int(cel_to_far(SS.get_temp()))
+
     date = time.strftime(DATE_FORMAT)
-    
+
     current_time = time.strftime(TIME_FORMAT)
-    
+
     reading = (date, current_time, temp, moisture)
-    
+
     reading_id = write_to_db(conn, reading)
-    
+
     print("Last row id: " + str(reading_id))
-    
+
     conn.close()
-    
+
     if(moisture < 400):
         send_email(moisture)
 
