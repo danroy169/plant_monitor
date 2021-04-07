6558ebaf6be97645c16181edcaa1457247b0920f
Daniel Roy <danielwroy90@gmail.com>
2021-04-07 15:43:50 -0400

    "buffer stuff"

--- moisture-i2c-js/index.js
+++ moisture-i2c-js/index.js
@@ -3,6 +3,8 @@ const sleep = require('sleep');
 
 const SENSOR_I2C_ADDRESS = 0x36;
 const I2C_BUS_NUMBER = 1;
+const buff = Buffer.alloc(2);
+const buffLength = buff.length;
 
 const i2c_bus = i2c.open(I2C_BUS_NUMBER, function(err) {
   if(err){
@@ -10,8 +12,13 @@ const i2c_bus = i2c.open(I2C_BUS_NUMBER, function(err) {
     process.exit(1);
   }
   console.log("I'm connected?");
+  i2c_bus.i2cRead(SENSOR_I2C_ADDRESS, 2, buff, function(err, buffLength, buff) {
+    console.log(buff.toJSON());
+  });
+ 
 }
 );
+
 
 
 
