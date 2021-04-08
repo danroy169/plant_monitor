1407df91a80cb5ef63cdcfc9aaa3262f462a33cd
Daniel Roy <daniel.roy@us.bosch.com>
2021-04-08 11:19:23 -0400

    message

--- moisture-i2c-js/index.js
+++ moisture-i2c-js/index.js
@@ -1,45 +1,41 @@
 const i2c = require('i2c-bus');
-const sleep = require('sleep');
+
 
 const SENSOR_I2C_ADDRESS = 0x36;
 const I2C_BUS_NUMBER = 1;
-const TOUCH_BASE = 0x0F;
-const TOUCH_CHANNEL_OFFSET = 0x10;
+// const TOUCH_BASE = 0x0F;
+// const TOUCH_CHANNEL_OFFSET = 0x10;
+
+async function main(){
+  const bus = await i2c.openPromisified(I2C_BUS_NUMBER);
+
+  await bus.i2cWrite(SENSOR_I2C_ADDRESS, 2, Buffer.from([ 0x00, 0x02 ]));
+
+  const returnValue = await bus.i2cRead(SENSOR_I2C_ADDRESS, 4, Buffer.alloc(4));
+
+  console.log(returnValue);
+}
+main();
 
 
-let buff = Buffer.alloc(2);
-const buffLength = buff.length;
-
-console.log("pre-buffer", buff);
-
-const i2c_bus = i2c.open(I2C_BUS_NUMBER, err => {
-  if(err){
-    console.log("Error opening i2c bus: ", err);
-    process.exit(1);
-  }
-
-  console.log("I'm connected?");
-  console.log(i2c_bus)
-  console.log(i2c_bus.i2cFuncsSync());
-
-  i2c_bus.i2cRead(SENSOR_I2C_ADDRESS, buffLength, buff, (err, buffLength, buff) => {
-    if(err) console.log(err);
-    console.log(buff.toJSON());
   
-  });
-});
 
+// const i2c_bus = i2c.open(I2C_BUS_NUMBER, err => {
+//   if(err){
+//     console.log("Error opening i2c bus: ", err);
+//     process.exit(1);
+//   }
 
+//   console.log("I'm connected?");
+//   console.log(i2c_bus)
+//   console.log(i2c_bus.i2cFuncsSync());
 
-
-
-
-
-
-
-
-
-
+//   i2c_bus.i2cRead(SENSOR_I2C_ADDRESS, buffLength, buff, (err, buffLength, buff) => {
+//     if(err) console.log(err);
+//     console.log(buff.toJSON());
+  
+//   });
+// });
 
 
 // def moisture_read(self):
