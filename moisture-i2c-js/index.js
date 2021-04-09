cc597d4b9e2bc566165f6eccad30eb1d9e89fb8e
Daniel Roy <danielwroy90@gmail.com>
2021-04-09 14:49:39 -0400

    "moisture reading"

--- moisture-i2c-js/index.js
+++ moisture-i2c-js/index.js
@@ -1,31 +1,43 @@
 const i2c = require('i2c-bus');
+const sleep = require("sleep")
 
 
 const SENSOR_I2C_ADDRESS = 0x36;
 const I2C_BUS_NUMBER = 1;
-// const TOUCH_BASE = 0x0F;
-// const TOUCH_CHANNEL_OFFSET = 0x10;
+const TOUCH_BASE = 0x0F;
+const TOUCH_CHANNEL_OFFSET = 0x10;
 
-const readBufferCache = Buffer.alloc(4);
+const readBufferCache = Buffer.alloc(2);
 
 async function main(){
   const bus = await i2c.openPromisified(I2C_BUS_NUMBER);
 
-  await bus.i2cWrite(SENSOR_I2C_ADDRESS, 2, Buffer.from([ 0x00, 0x02 ]));
+  await bus.i2cWrite(SENSOR_I2C_ADDRESS, 2, Buffer.from([ TOUCH_BASE, TOUCH_CHANNEL_OFFSET ]));
 
-  const returnValue = await bus.i2cRead(SENSOR_I2C_ADDRESS, 4, readBufferCache);
+  sleep.msleep(10);
+
+  const returnValue = await bus.i2cRead(SENSOR_I2C_ADDRESS, 2, readBufferCache);
+
+  console.log(convertBufferSourceToMoisture(returnValue.buffer));
 
   //console.log(returnValue.buffer.toJSON()); { bytesRead: 4, buffer: <Buffer 0f ba 27 8f> }
 
-  const version = convertBufferToVersion(returnValue.buffer);
+  // const version = convertBufferToVersion(returnValue.buffer);
 
-  const version2 = convertArrayBufferToVersion(returnValue.buffer.buffer);
+  // const version2 = convertArrayBufferToVersion(returnValue.buffer.buffer);
 
-  const version3 = convertArrayBufferToVersion2(returnValue.buffer.buffer);
+  // const version3 = convertArrayBufferToVersion2(returnValue.buffer.buffer);
 
-  const version4 = convertBufferSourceToVersion(returnValue.buffer.buffer);
+  // const version4 = convertBufferSourceToVersion(returnValue.buffer.buffer);
 
-  console.log({ version, version2, version3, version4 });
+  // console.log({ version, version2, version3, version4 });
+
+  // {
+  //   version: 263858063,
+  //   version2: 2401745423,
+  //   version3: 263858063,
+  //   version4: 263858063
+  // }
 }
 main();
 
@@ -56,4 +68,12 @@ function convertBufferSourceToVersion(bs){
     new DataView(bs)
 
   return dv.getUint32(0, false);
+}
+
+function convertBufferSourceToMoisture(buf){
+  const dv = ArrayBuffer.isView(buf) ?
+    new DataView(buf.buffer, buf.byteOffset, buf.byteLength) :
+    new DataView(buf)
+
+  return dv.getUint16(0, false);
 }
