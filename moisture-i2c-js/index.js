42b17f96d08d7173f6caba151d1056b98fb434d3
Daniel Roy <daniel.roy@us.bosch.com>
2021-04-09 13:15:53 -0400

    buffer

--- moisture-i2c-js/index.js
+++ moisture-i2c-js/index.js
@@ -6,54 +6,54 @@ const I2C_BUS_NUMBER = 1;
 // const TOUCH_BASE = 0x0F;
 // const TOUCH_CHANNEL_OFFSET = 0x10;
 
+const readBufferCache = Buffer.alloc(4);
+
 async function main(){
   const bus = await i2c.openPromisified(I2C_BUS_NUMBER);
 
   await bus.i2cWrite(SENSOR_I2C_ADDRESS, 2, Buffer.from([ 0x00, 0x02 ]));
 
-  const returnValue = await bus.i2cRead(SENSOR_I2C_ADDRESS, 4, Buffer.alloc(4));
+  const returnValue = await bus.i2cRead(SENSOR_I2C_ADDRESS, 4, readBufferCache);
 
-  console.log(returnValue.buffer.toJSON()); // { bytesRead: 4, buffer: <Buffer 0f ba 27 8f> }
+  //console.log(returnValue.buffer.toJSON()); { bytesRead: 4, buffer: <Buffer 0f ba 27 8f> }
+
+  const version = convertBufferToVersion(returnValue.buffer);
+
+  const version2 = convertArrayBufferToVersion(returnValue.buffer.buffer);
+
+  const version3 = convertArrayBufferToVersion2(returnValue.buffer.buffer);
+
+  const version4 = convertBufferSourceToVersion(returnValue.buffer.buffer);
+
+  console.log({ version, version2, version3, version4 });
 }
 main();
 
 
   
-
-// const i2c_bus = i2c.open(I2C_BUS_NUMBER, err => {
-//   if(err){
-//     console.log("Error opening i2c bus: ", err);
-//     process.exit(1);
-//   }
-
-//   console.log("I'm connected?");
-//   console.log(i2c_bus)
-//   console.log(i2c_bus.i2cFuncsSync());
-
-//   i2c_bus.i2cRead(SENSOR_I2C_ADDRESS, buffLength, buff, (err, buffLength, buff) => {
-//     if(err) console.log(err);
-//     console.log(buff.toJSON());
-  
-//   });
-// });
+// quick dirty buffer version
+function convertBufferToVersion(buffer) {
+  return buffer.readUInt32BE(0)
+}
 
 
-// def moisture_read(self):
-// """Read the value of the moisture sensor"""
-// buf = bytearray(2)
+// proper - fail
+function convertArrayBufferToVersion(aBuffer) {
+  const a = new Uint32Array(aBuffer);
+  return a[0]
+  // assumes BE platform (incorrectly)
+}
 
-// self.read(_TOUCH_BASE, _TOUCH_CHANNEL_OFFSET, buf, 0.005)
-// ret = struct.unpack(">H", buf)[0]
-// time.sleep(0.001)
+// structural fail - not flexible
+function convertArrayBufferToVersion2(aBuffer) {
+  const dv = new DataView(aBuffer);
+  return dv.getUint32(0, false);
+}
 
-// # retry if reading was bad
-// count = 0
-// while ret > 4095:
-//     self.read(_TOUCH_BASE, _TOUCH_CHANNEL_OFFSET, buf, 0.005)
-//     ret = struct.unpack(">H", buf)[0]
-//     time.sleep(0.001)
-//     count += 1
-//     if count > 3:
-//         raise RuntimeError("Could not get a valid moisture reading.")
+function convertBufferSourceToVersion(bs){
+  const dv = ArrayBuffer.isView(bs) ? 
+    new DataView(bs.buffer, bs.byteOffset, bs.byteLength) :
+    new DataView(bs)
 
-// return ret
+  return dv.getUint32(0, false);
+}
