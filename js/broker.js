1bba746c1e0d9b14527887805475c1dd3f4fae93
Daniel Roy <danielwroy90@gmail.com>
2021-04-11 17:39:12 -0400

    refactor

--- js/broker.js
+++ js/broker.js
@@ -3,7 +3,7 @@ const server = require('net').createServer(aedes.handle)
 const port = 8883
 
 server.listen(port, function () {
-  console.log('server started and listening on port ', port)
+    console.log('server started and listening on port ', port)
 })
 
 aedes.on('client', () => {
@@ -19,7 +19,7 @@ aedes.on("subscribe", (sub, client) => {
 })
 
 aedes.on("publish", (packet, client) => {
-    if (packet.topic == 'moisture') {
+    if(packet.topic === "moisture"){
         var stringBuf = packet.payload.toString('utf-8');
         var obj = JSON.parse(stringBuf);
         console.log(obj);
