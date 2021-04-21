d0c4496afbf03bd86a0a285c827607092a4bf6b7
Daniel Roy <daniel.roy@us.bosch.com>
2021-04-21 10:44:45 -0400

    metric. refactor

--- js/metric-service.js
+++ js/metric-service.js
@@ -1,5 +1,5 @@
 import { connect } from "async-mqtt"
-import { DATA_REQUEST, DATA_RESPONSE, SENSOR_RESPONSE, URL } from "./consts.js"
+import { DATA_REQUEST, DATA_RESPONSE, SENSOR_RESPONSE, URL, MOISTURE, TEMP, HUMIDITY } from "./consts.js"
 
 const client = connect(URL);
 
@@ -11,3 +11,13 @@ client.on("connect", () => {
     console.log("connected")
     client.subscribe([DATA_REQUEST, SENSOR_RESPONSE])
 });
+
+client.on("message", (topic, message, packet) => {
+    let obj = JSON.parse(message.toString())
+
+    if(obj.type === MOISTURE)  {moistureReadings.push(obj)}
+    if(obj.type === TEMP)  {tempReadings.push(obj)}
+    if(obj.type === HUMIDITY)  {humidReadings.push(obj)}
+
+    console.log(moistureReadings)
+})
--- js/validator.js
+++ js/validator.js
@@ -12,59 +12,59 @@ export default function validateJSON(obj){
     return result;
 }
 
-const test = [{
-    "sensorID": "moisture1",
-    "time": "2012-01-26T13:51:50.417-07:00",
-    "type": "moisture",
-    "moistureLevel": 330,
-    "currentPollInterval": 5
+// const test = [{
+//     "sensorID": "moisture1",
+//     "time": "2012-01-26T13:51:50.417-07:00",
+//     "type": "moisture",
+//     "moistureLevel": 330,
+//     "currentPollInterval": 5
 
-},
-{
-    "sensorID": "temp-humid",
-    "type": "temp",
-    "time": "2012-01-26T13:51:50.417-07:00",
-},
-{
-    "target": "moisture1",
-    "setting": "pollInterval",
-    "data": 30,
-    "time": "2012-01-26T13:51:50.417-07:00"
-},
-{
-    "target": "moisture1",
-    "result": true,
-    "time": "2012-01-26T13:51:50.417-07:00"
-},
-{
-    "sensorID": "moisture2",
-    "violationType": "moisture",
-    "threshold": 330,
-    "currentLevel": 300,
-    "time": "2012-01-26T13:51:50.417-07:00"
-},
-{
-    "sensorID": "temp-humid",
-    "violationType": "humidity",
-    "threshold": 75,
-    "reading": 80,
-    "time": "2012-01-26T13:51:50.417-07:00"
-},
-{
-    "result": true,
-    "time": "2012-01-26T13:51:50.417-07:00"
-},
-{
-    "metric": "moisture1",
-    "numberOfReadings": 10,
-    "time": "2012-01-26T13:51:50.417-07:00"
-},
-{
-    "metric": "moisture1",
-    "result":[ { "moistureLevel": 330, "time": "2012-01-26T13:51:50.417-07:00" } ],
-    "time": "2012-01-26T13:51:50.417-07:00"
-}
-]
+// },
+// {
+//     "sensorID": "temp-humid",
+//     "type": "temp",
+//     "time": "2012-01-26T13:51:50.417-07:00",
+// },
+// {
+//     "target": "moisture1",
+//     "setting": "pollInterval",
+//     "data": 30,
+//     "time": "2012-01-26T13:51:50.417-07:00"
+// },
+// {
+//     "target": "moisture1",
+//     "result": true,
+//     "time": "2012-01-26T13:51:50.417-07:00"
+// },
+// {
+//     "sensorID": "moisture2",
+//     "violationType": "moisture",
+//     "threshold": 330,
+//     "currentLevel": 300,
+//     "time": "2012-01-26T13:51:50.417-07:00"
+// },
+// {
+//     "sensorID": "temp-humid",
+//     "violationType": "humidity",
+//     "threshold": 75,
+//     "reading": 80,
+//     "time": "2012-01-26T13:51:50.417-07:00"
+// },
+// {
+//     "result": true,
+//     "time": "2012-01-26T13:51:50.417-07:00"
+// },
+// {
+//     "metric": "moisture1",
+//     "numberOfReadings": 10,
+//     "time": "2012-01-26T13:51:50.417-07:00"
+// },
+// {
+//     "metric": "moisture1",
+//     "result":[ { "moistureLevel": 330, "time": "2012-01-26T13:51:50.417-07:00" } ],
+//     "time": "2012-01-26T13:51:50.417-07:00"
+// }
+// ]
 
 
 // test.forEach(t => {
