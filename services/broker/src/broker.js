8268952f992613edd42d62027f73f8722e9beb9f
Daniel Roy <daniel.roy@us.bosch.com>
2021-04-30 12:49:39 -0400

    broker refactor

--- services/broker/src/broker.js
+++ services/broker/src/broker.js
@@ -1,39 +1,18 @@
 import Aedes from 'aedes'
 import net from 'net'
-
-import validateJSON from '../../../util/validator.js'
-import { PORT, TOPICS } from '../../../util/consts.js'
+import { PORT } from '../../../util/consts.js'
 
 
 // import validateJSON from "/home/pi/Projects/Plant Monitor/js/validator.js"
 // import { PORT, TOPICS } from "/home/pi/Projects/Plant Monitor/js/consts.js"
 
 const broker = new Aedes()
+
 const server = net.createServer(broker.handle)
 
-
 server.listen(PORT, function () {
-    console.log('server started and listening on port ', PORT)
+    console.log('MQTT server started and listening on port ', PORT)
 })
 
 
 
-broker.on('clientDisconnect', () => {
-    console.log('client disconnected\n')
-})
-
-// broker.on("subscribe", (sub, client) => {
-//     sub.forEach(obj => {
-//         console.log(`${client.id} subscribed to ${obj.topic}\n`);
-//     }) 
-// })
-
-broker.on('publish', (packet) => {
-    //console.log(`publish packet recieved. topic: ${packet.topic}\n`)
-    if (TOPICS.includes(packet.topic)) {
-        var stringBuf = packet.payload.toString('utf-8')
-        var obj = JSON.parse(stringBuf)
-  
-        if(!validateJSON(obj)) {console.log('invalid message!\n')}
-    }
-})
