4989491a48a793d90993a5b3aeb70821ed656327
Daniel Roy <danielwroy90@gmail.com>
2021-05-03 12:45:42 -0400

    basic express set up. latest readings

--- services/gateway/src/service-gateway.js
+++ services/gateway/src/service-gateway.js
@@ -1,29 +1,57 @@
 import { parentPort } from 'worker_threads'
-import { CONFIG_REQUEST, DATA_REQUEST, DATA_RESPONSE, HUMIDITY, MESSAGE, MOISTURE, MOISTURE_LOW, MOISTURE_SENSOR_1, TEMP, THRESHOLD_SERVICE } from '../../../util/consts.js'
+import { DATA_REQUEST, DATA_RESPONSE, HUMIDITY, MESSAGE, MOISTURE_SENSOR_1, TEMP } from '../../../util/consts.js'
 import isValidMessage from '../../../util/validator.js'
-// import { DATA_REQUEST, DATA_RESPONSE, SENSOR_RESPONSE, SENSOR_REQUEST, CONFIG_REQUEST, CONFIG_RESPONSE, URL } from "/home/pi/Projects/Plant Monitor/js/consts.js"
+import  express  from 'express'
 
+const port = 3000
+const app = express()
 
-
-// const configRequest = {
-//     topic: CONFIG_REQUEST,
-//     target: THRESHOLD_SERVICE,
-//     setting: MOISTURE_LOW,
-//     data: 400,
-//     time: new Date().toISOString()
-// }
-
-const dataRequest = {
+const tempDataRequest = {
     topic: DATA_REQUEST,
     metric: TEMP,
-    numberOfReadings: 2,
+    numberOfReadings: 1,
     time: new Date().toISOString()
 }
 
+const humidDataRequest = {
+    topic: DATA_REQUEST,
+    metric: HUMIDITY,
+    numberOfReadings: 1,
+    time: new Date().toISOString()
+}
+
+const moisture1DataRequest = {
+    topic: DATA_REQUEST,
+    metric: MOISTURE_SENSOR_1,
+    numberOfReadings: 1,
+    time: new Date().toISOString()
+}
+
+let latestReadings = {}
 
 parentPort.on(MESSAGE, msg => { 
     console.log('Gateway Service recieved', msg.topic, 'message\n')
-    if(msg.topic === DATA_RESPONSE) {console.log(msg)} })
 
-setTimeout(() => { if(isValidMessage(dataRequest)) { parentPort.postMessage(dataRequest) } }, 8000)
+    if(msg.topic === DATA_RESPONSE && msg.metric === HUMIDITY) { latestReadings.humidity = msg } 
+    if(msg.topic === DATA_RESPONSE && msg.metric === TEMP) { latestReadings.temp = msg } 
+    if(msg.topic === DATA_RESPONSE && msg.metric === MOISTURE_SENSOR_1) { latestReadings.moisture1 = msg } 
+
+})
+
+app.get('/api/latest-readings', (req, res) => {
+    if(isValidMessage(tempDataRequest)) { parentPort.postMessage(tempDataRequest) } 
+    if(isValidMessage(humidDataRequest)) { parentPort.postMessage(humidDataRequest) }
+    if(isValidMessage(moisture1DataRequest)) { parentPort.postMessage(moisture1DataRequest) }
+
+    res.json(latestReadings)
+})
+
+
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:' + port)
+})
+
+
+
+
 
