c9db2540deeada39c147270cec10954b4fc846d8
Daniel Roy <daniel.roy@us.bosch.com>
2021-05-05 16:16:54 -0400

    refactor

--- site/script.js
+++ site/script.js
@@ -1,64 +1,19 @@
-async function getReadings() {
-    const response = await fetch('http://localhost:3000/api/latest-readings', { mode: 'cors' })
-
-    const readings = await response.json()
-
-    console.log(readings)
-
-    return readings
-}
-
 async function setupSSE() {
-    const moistureReading = document.getElementById("moisture")
-    const tempReading = document.getElementById("temp")
-    const humidReading = document.getElementById("humid")
-
-    const moistureTime = document.getElementById('moistureTime')
-    const tempTime = document.getElementById('tempTime')
-    const humidTime = document.getElementById('humidTime')
-
     const evtSource = new EventSource("http://localhost:3030/sse");
 
-    evtSource.onmessage = event => {
-        let message = JSON.parse(event.data)
-
-        console.log(message)
-
-        if(message.fahrenheit) { tempReading.innerText = message.fahrenheit; tempTime.innerText = new Date(message.time).toLocaleTimeString()}
-        if(message.percent) { humidReading.innerText = message.percent; humidTime.innerText = new Date(message.time).toLocaleTimeString()}
-        if(message.moistureLevel) { moistureReading.innerText = message.moistureLevel; moistureTime.innerText = new Date(message.time).toLocaleTimeString()}
+    const elements = {
+        moistureReading: document.getElementById("moisture"),
+        moistureTime: document.getElementById('moistureTime'),
+        tempReading: document.getElementById('temp'),
+        tempTime: document.getElementById('tempTime'),
+        humidReading: document.getElementById('humid'),
+        humidTime: document.getElementById('humidTime')
     }
 
-    evtSource.onopen = event => {
-        console.log('open')
-    }
+    evtSource.onmessage = event => { onMessage(event, elements) }
 }
 
-async function init() {
-    // const moistureReading = document.getElementById("moisture")
-    // const tempReading = document.getElementById("temp")
-    // const humidReading = document.getElementById("humid")
-
-    // const moistureTime = document.getElementById('moistureTime')
-    // const tempTime = document.getElementById('tempTime')
-    // const humidTime = document.getElementById('humidTime')
-
-    //const readings = await getReadings()
-
-    // moistureReading.innerText = readings.moisture1.result[0].moistureLevel
-
-    // tempReading.innerText = readings.temp.result[0].fahrenheit
-
-    // humidReading.innerText = readings.humidity.result[0].percent
-
-    // moistureTime.innerText = new Date(readings.moisture1.result[0].time).toLocaleTimeString()
-
-    // tempTime.innerText = new Date(readings.temp.result[0].time).toLocaleTimeString()
-
-    // humidTime.innerText = new Date(readings.humidity.result[0].time).toLocaleTimeString()
-
-    await setupSSE()
-}
+async function init() { await setupSSE() }
 
 if(document.readyState === 'loading') { 
     console.log('loading')
@@ -66,4 +21,25 @@ if(document.readyState === 'loading') {
 }
 else { 
     init()
+}
+
+
+function onMessage(event, elements){
+    const message = JSON.parse(event.data)
+
+    if(message.fahrenheit) { 
+        elements.tempReading.innerText = message.fahrenheit
+        elements.tempTime.innerText = new Date(message.time).toLocaleTimeString() 
+    }
+
+    if(message.percent) { 
+        elements.humidReading.innerText = message.percent
+        humidTime.innerText = new Date(message.time).toLocaleTimeString() 
+    }
+
+    if(message.moistureLevel) { 
+        elements.moistureReading.innerText = message.moistureLevel
+        elements.moistureTime.innerText = new Date(message.time).toLocaleTimeString() 
+    }
+
 }
