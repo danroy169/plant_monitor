ba707d1d20655e2d5ff3675ff9bc985f9c04b1aa
Roy Daniel (C/HRR3.2-NA) <danielwroy90@gmail.com>
2021-05-10 16:12:40 -0400

    validator tests

---
+++ util/test/validator.test.js
@@ -0,0 +1,36 @@
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+import { SENSOR_RESPONSE, TEMP, TEMP_HUMIDITY_SENSOR } from '../consts.js'
+import isAValidMessage from '../validator.js'
+
+describe('isAValidMessage(msg)', () => {
+    
+    it('should return true when given a valid message', () => {
+
+        const msg = { 
+            time: new Date().toISOString(),
+            topic: SENSOR_RESPONSE,
+            sensorID: TEMP_HUMIDITY_SENSOR,
+            type: TEMP,
+            fahrenheit: 350,
+            currentPollInterval: 5
+         }
+
+         expect(isAValidMessage(msg)).to.be.true
+    })
+
+    it('should return false when given an invalid message', () => {
+
+        // wront date format
+        const msg = { 
+            time: new Date(),
+            topic: SENSOR_RESPONSE,
+            sensorID: TEMP_HUMIDITY_SENSOR,
+            type: TEMP,
+            fahrenheit: 350,
+            currentPollInterval: 5
+         }
+
+        expect(isAValidMessage(msg)).to.be.false
+    })
+})
