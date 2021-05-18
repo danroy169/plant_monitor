e30c95a3fbb4dc003c05b27cc920f8bfe71ef094
Daniel Roy <danielwroy90@gmail.com>
2021-05-18 15:44:48 -0400

    averages setup

--- site/public/get-metric.js
+++ site/public/get-metric.js
@@ -1,7 +1,15 @@
-export default async function getMetrics(metricID, amount){
+export async function getMetrics(metricID, amount){
     const request = await fetch('http://192.168.0.117:3000/api/metric/' + metricID + '/amount/' + amount, {mode: 'cors'})
     
     const response = await request.json()
 
     return response
+}
+
+export async function getAverage(metricID) {
+    const response = await getMetrics(metricID, 'average')
+
+    const average = Math.round(response.result[0])
+
+    return average
 }
--- site/public/render.js
+++ site/public/render.js
@@ -1,77 +1,44 @@
-import getMetrics from './get-metric.js'
+import { getAverage } from './get-metric.js'
 import setupSSE from './sse-setup.js'
 
 async function init() { 
 
     await setupSSE() 
 
-    const moisture1Button = document.getElementById('getAverageMoisture1')
-    moisture1Button.addEventListener('click', displayMoisture1Average)
+    displayMoisture1Average()
+    displayMoisture2Average()
+    displayHumidAverage()
+    displayTempAverage()
 
-    const moisture2Button = document.getElementById('getAverageMoisture2')
-    moisture2Button.addEventListener('click', displayMoisture2Average)
-
-    const tempButton = document.getElementById('getAverageTemp')
-    tempButton.addEventListener('click', displayTempAverage)
-
-    const humidButton = document.getElementById('getAverageHumid')
-    humidButton.addEventListener('click', displayHumidAverage)
 }
 
 async function displayMoisture1Average(){
-    const response = await getMetrics('moisture1', 'average')
 
-    const average = Math.round(response.result[0])
+    const average = await getAverage('moisture1')
 
-    const div = document.getElementById('moisture1Div')
-    
-    const p = document.createElement('p')
-
-    p.innerText = average
-
-    div.appendChild(p)
+    document.getElementById('moisture1Avg').innerText = average
 }
 
 async function displayMoisture2Average(){
-    const response = await getMetrics('moisture2', 'average')
 
-    const average = Math.round(response.result[0])
+    const average = await getAverage('moisture2')
 
-    const div = document.getElementById('moisture2Div')
+    document.getElementById('moisture2Avg').innerText = average
     
-    const p = document.createElement('p')
-
-    p.innerText = average
-
-    div.appendChild(p)
 }
 
 async function displayTempAverage(){
-    const response = await getMetrics('temp', 'average')
 
-    const average = Math.round(response.result[0])
+    const average = await getAverage('temp')
 
-    const div = document.getElementById('tempDiv')
-    
-    const p = document.createElement('p')
-
-    p.innerText = average
-
-    div.appendChild(p)
+    document.getElementById('tempAvg').innerText = average
 }
 
 async function displayHumidAverage(){
-    const response = await getMetrics('humidity', 'average')
 
-    const average = Math.round(response.result[0])
+    const average = await getAverage('humidity')
 
-    const div = document.getElementById('humidDiv')
-    
-    const p = document.createElement('p')
-
-    p.innerText = average
-
-    div.appendChild(p)
+    document.getElementById('humidAvg').innerText = average
 }
 
 
