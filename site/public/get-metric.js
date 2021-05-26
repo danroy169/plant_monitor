befe86043ea92f91c541f02a2b031df3affac6c6
Daniel Roy <danielwroy90@gmail.com>
2021-05-26 19:52:36 -0400

    reconfig for work

--- site/public/get-metric.js
+++ site/public/get-metric.js
@@ -1,6 +1,6 @@
 export async function getMetrics(metricID, amount){
-    const request = await fetch('http://192.168.0.117:3000/api/metric/' + metricID + '/amount/' + amount, {mode: 'cors'})
-    //const request = await fetch('http://localhost:3000/api/metric/' + metricID + '/amount/' + amount, {mode: 'cors'})
+    //const request = await fetch('http://192.168.0.117:3000/api/metric/' + metricID + '/amount/' + amount, {mode: 'cors'})
+    const request = await fetch('http://localhost:3000/api/metric/' + metricID + '/amount/' + amount, {mode: 'cors'})
     
     const response = await request.json()
 
